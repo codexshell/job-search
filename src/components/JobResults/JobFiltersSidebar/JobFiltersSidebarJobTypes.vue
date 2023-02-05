ecdf4fc1c0b7618e6dd1b958a513582d51dff33b
Silvanos Eric <ericoderito@gmail.com>
2023-02-05 20:38:49 +0000

    refactor: migrate JobFiltersSidebarJobTypes component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
@@ -5,26 +5,11 @@
   />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
+<script setup lang="ts">
 import { useUniqueJobTypes } from "@/store/composables";
 import { ADD_SELECTED_JOB_TYPES } from "@/store/constants";
 
 import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
 
-export default defineComponent({
-  name: "JobFiltersSidebarJobTypes",
-  components: {
-    JobFiltersSidebarCheckboxGroup,
-  },
-  setup() {
-    const uniqueJobTypes = useUniqueJobTypes();
-
-    return {
-      uniqueJobTypes,
-      ADD_SELECTED_JOB_TYPES,
-    };
-  },
-});
+const uniqueJobTypes = useUniqueJobTypes();
 </script>
