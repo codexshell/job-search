dd1029f3baf765eaff0ad5e3d5f6c03712156773
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-02-07 23:24:04 +0300

    chore: migrate ProfileImage component to Vue 3

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -2,18 +2,7 @@
   <img :src="imageLink" alt="Profile Image" class="w-8 rounded-3xl" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "ProfileImage",
-  setup() {
-    const imageLink =
-      "https://www.pngitem.com/pimgs/m/487-4876417_link-head-png-toon-link-face-png-transparent.png";
-
-    return {
-      imageLink,
-    };
-  },
-});
+<script setup lang="ts">
+const imageLink =
+  "https://www.pngitem.com/pimgs/m/487-4876417_link-head-png-toon-link-face-png-transparent.png";
 </script>
