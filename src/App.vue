cc1051cc708f3af5c19726ecb0995c1096d3a13d
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-02-07 23:48:52 +0300

    chore: migrate App component to Vue 3

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,6 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
+<script setup lang="ts">
 import MainNav from "@/components/Navigation/MainNav.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    MainNav,
-  },
-});
 </script>
